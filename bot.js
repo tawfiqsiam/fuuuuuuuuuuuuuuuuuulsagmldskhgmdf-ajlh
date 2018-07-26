5ab5bb624e09eefc7ab905757e8b1ce12208b0f1
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-07-26 17:56:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js");RichEmbed
 const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
 const dateFormat = require('dateformat');
